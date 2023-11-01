35cf8cd78470d111b34f5f960accd47dd07f4560
yuke <yuke@cs.stonybrook.edu>
2023-11-01 17:22:30 -0400

    Update index.js to use express, and auth-router

--- server/index.js
+++ server/index.js
@@ -1,14 +1,26 @@
-const http = require('http');
+const http = require('http')
+const express = require('express')
+const cors = require('cors')
+const cookieParser = require('cookieparser')
 
-const hostname = '127.0.0.1';
-const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
+const hostname = '127.0.0.1'
+const port = 3000
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+const app = express()
+
+// MIDDLE WARE
+app.use(express.urlencoded({extend: true}))
+app.use(cors({
+  origin: ["http://localhost:3000"],
+  credentials: true
+}))
+app.use(express.json())
+// app.use(cookieParser())
+
+const authRouter = require('./routes/auth-router')
+app.use('/auth', authRouter)
+
+
+// Run server
+app.listen(port, () => console.log(`Server running on port ${port}`))
