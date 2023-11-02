071cfbfde668faeeef5bf940f20d3213334aa0cf
yuke <yuke@cs.stonybrook.edu>
2023-11-01 21:51:20 -0400

    sample response in auth-controller

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -1,18 +1,66 @@
 const auth = require('../auth') // This is the auth manager, so some code should be abstracted to this file.
 const User = require('../schemas/userProfileSchema')
 
+
 loggedIn = async (req, res) => {
-    return res.status(404).json({
-        loggedIn: false,
-        user: null,
-        errorMessage: "?"
+    return res.status(200).json({
+        username: "Test",
+        email: "test@test.com",
+        picture: ""
     })
 }
-login = async (req, res) => {}
-logout = async (req, res) => {}
-register = async (req, res) => {}
-resetRequest = async (req, res) => {}
-verifyCode = async (req, res) => {}
+
+login = async (req, res) => {
+    // Get info from req.
+
+    // Send to auth manager
+
+    // Return to user
+    return res.status(200).json({
+        username: "Test-login",
+        email: "test@test.com",
+        picture: ""
+    })
+}
+
+logout = async (req, res) => {
+    // Logsout
+    // Invalid session
+
+    return res.status(200).json({
+        // Set cookie to expire
+    })
+}
+
+register = async (req, res) => {
+    // Get information from req
+
+    // Send to auth manager
+
+    // Return to user
+    return res.status(200).json({
+        username: "Test-register",
+        email: "test@test.com",
+        picture: ""
+    })
+}
+
+resetRequest = async (req, res) => {
+    // Get info from req
+
+    // Send to auth manager
+
+    // Return status
+}
+
+verifyCode = async (req, res) => {
+    // Get code from req
+
+    // Send to auth manager
+
+    // Return status
+}
+
 updatePasscode = async (req, res) => {}
 updateUsername = async (req, res) => {}
 updateEmail = async (req, res) => {}
