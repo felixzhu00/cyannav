cc8e02d572802ba6309c2a01bb7c7b12276ac91c
Tommy <lin.tommy@outlook.com>
2023-11-10 17:15:44 -0500

    Added Inter font to our app

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,24 @@ import './App.css';
 import APITester from './components/APITester.jsx';
 import Map from './components/Map.jsx'
 import React from 'react';
+import AppBanner from './components/AppBanner.jsx'
+import LoginScreen from './components/LoginScreen.jsx'
+import { createTheme, ThemeProvider } from '@mui/material';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      'Inter',
+    ].join(','),
+  },
+});
 
 function App() {
   return (
-    <APITester/>
+    <ThemeProvider theme={theme}>
+      <LoginScreen />
+
+    </ThemeProvider>
   );
 }
 
