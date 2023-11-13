d7e65fdec1b638bfb3e6764c3d966400128d2fd1
felixzhu00 <felix.zhu00@gmail.com>
2023-11-12 20:09:43 -0500

    fix going to profile screen

--- client/src/components/AppBanner.jsx
+++ client/src/components/AppBanner.jsx
@@ -151,7 +151,7 @@ function AppBanner() {
                 onClose={handleCloseUserMenu}
               >
                 {settings.map((setting) => (
-                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <MenuItem key={setting} onClick={()=>{handleCloseUserMenu(setting)}}>
                     <Typography textAlign="center">{setting}</Typography>
                   </MenuItem>
                 ))}
