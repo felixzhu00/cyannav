ed13126c71926a5f4b050443b817268aaad3d0b9
yuke <yuke@cs.stonybrook.edu>
2023-11-15 16:53:22 -0500

    Most of auth apis implemented

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -1,24 +1,31 @@
-const auth = require('../auth') // This is the auth manager, so some code should be abstracted to this file.
-const User = require('../schemas/userProfileSchema')
+const auth = require("../auth") // This is the auth manager, so some code should be abstracted to this file.
+const User = require("../schemas/userProfileSchema")
 
+const bcrypt = require("bcrypt")
+const saltRounds = 10
 
 loggedIn = async (req, res) => {
-        try {
-            console.log('test')
+    try {
+        console.log("test")
         console.log("auth-controller::loggedIn")
         console.log("req: " + req)
 
-        let userId = auth.verifyUser(req);
+        let userId = auth.verifyUser(req)
         if (!userId) {
             return res.status(200).json({
                 loggedIn: false,
-                user: null
+                user: null,
+                errorMessage: "Invalid username or password.",
             })
         }
 
-        const loggedInUser = await User.findOne({_id: userId});
+        const loggedInUser = await User.findOne({ _id: userId })
         if (!loggedInUser) {
-            // TODO: deal with this.
+            return res.status(401).json({
+                loggedIn: false,
+                user: null,
+                errorMessage: "Invalid username or password.",
+            })
         }
 
         return res.status(200).json({
@@ -26,15 +33,15 @@ loggedIn = async (req, res) => {
             user: {
                 username: loggedInUser.username,
                 email: loggedInUser.email,
-                picture: null // TODO: figure out profile picture
-            }
+                picture: null, // TODO: figure out profile picture
+            },
         })
     } catch (err) {
         console.log(err)
         // TODO: revoke user token?
         return res.status(200).json({
             loggedIn: false,
-            user: null
+            user: null,
         })
     }
 }
@@ -47,39 +54,56 @@ login = async (req, res) => {
             return res.status(400).json({
                 loggedIn: false,
                 user: null,
-                errorMessage: "Required fields empty."
+                errorMessage: "Required fields empty.",
             })
         }
 
         // Find user
-        const targetUser = await User.findOne({email: email})
+        const targetUser = await User.findOne({ email: email })
         if (!targetUser) {
             return res.status(401).json({
                 loggedIn: false,
                 user: null,
-                errorMessage: "Wrong email or password"
+                errorMessage: "Wrong email or password",
             })
         }
 
-        // TODO: Hash password can compare here, plaintext for BUILD#1
-        if (password != targetUser.password) {
-            return res.status(401).json({
-                loggedIn: false,
-                user: null,
-                errorMessage: "Wrong email or password"
-            })
-        }
+        bcrypt.compare(password, targetUser.password, function (result) {
+            if (err) {
+                // TODO: deal with this better.
+                return res.status(500).json({
+                    loggedIn: false,
+                    user: null,
+                    errorMessage: "Internal server error.",
+                })
+            }
 
-
-        // TODO-NOW: Sign tokens
-        return res.status(200).json({
-            loggedIn: true,
-            user: {
-                username: targetUser.username,
-                email: targetUser.email,
-                picture: null // TODO: figure out profile picture
+            if (!result) {
+                return res.status(401).json({
+                    loggedIn: false,
+                    user: null,
+                    errorMessage: "Wrong email or password",
+                })
             }
         })
+
+        const token = auth.signToken(targetUser._id)
+
+        return res
+            .cookie("access_token", token, {
+                httpOnly: true, // TODO: change this later when HTTPS is introduced.
+                secure: true,
+                sameSite: true,
+            })
+            .status(200)
+            .json({
+                loggedIn: true,
+                user: {
+                    username: targetUser.username,
+                    email: targetUser.email,
+                    picture: null, // TODO: figure out profile picture
+                },
+            })
     } catch (err) {
         console.log(err)
 
@@ -87,7 +111,7 @@ login = async (req, res) => {
         return res.status(200).json({
             loggedIn: false,
             user: null,
-            errorMessage:"TODO-login"
+            errorMessage: "TODO-login",
         })
     }
 }
@@ -95,56 +119,87 @@ login = async (req, res) => {
 register = async (req, res) => {
     try {
         const { email, username, password, passwordVerify } = req.body
-    
-        // Don't know if this should be handled on front-end
+
         if (password != passwordVerify) {
             return res.status(401).json({
                 loggedIn: false,
                 user: null,
-                errorMessage: "Passwords does not match"
+                errorMessage: "Passwords does not match",
             })
         }
-        // Maybe more password checks.
+
+        // TODO: go over concrete to stay consistent with front-end.
+        if (
+            password.length < 9 ||
+            [...password.matchAll(/[0-9]/g)].length == 0 || // numbers
+            [...password.matchAll(/\W/g)].length < 2 // Special characters
+        ) {
+            return res.status(401).json({
+                loggedIn: false,
+                user: null,
+                errorMessage: "Password fails security requirement.",
+            })
+        }
 
         // Check if email is already used
-        var existingUser = await User.findOne({email: email})
+        var existingUser = await User.findOne({ email: email })
         if (existingUser) {
             return res.status(401).json({
                 loggedIn: false,
                 user: null,
-                errorMessage: "Email is already in use"
+                errorMessage: "Email is already in use",
             })
         }
 
         // Check if username is already used
-        existingUser = await User.findOne({username: username})
+        existingUser = await User.findOne({ username: username })
         if (existingUser) {
             return res.status(401).json({
                 loggedIn: false,
                 user: null,
-                errorMessage: "Username is already taken"
+                errorMessage: "Username is already taken",
             })
         }
 
-        // TODO: Hashpassword
+        var hashed_password
+        bcrypt.hash(password, saltRounds, function (err, hash) {
+            if (err) {
+                return res.status(500).json({
+                    loggedIn: false,
+                    errorMessage: "Internal server error.",
+                })
+            }
 
-        const newUser = new User({email, password, username})
+            hashed_password = hash
+        })
+
+        const newUser = new User({ email, hashed_password, username })
         const saved = await newUser.save()
 
         if (!saved) {
-            // TODO: Failed to save new user.
+            return res.status(500).json({
+                loggedIn: false,
+                errorMessage: "Internal server error.",
+            })
         }
 
-        // TODO: Sign toekn.
+        const token = auth.signToken(newUser._id)
 
-        return res.status(200).json({
-            loggedIn: true,
-            user: {
-                username: saved.username,
-                email: saved.email,
-                picture: null // Since new user have no profile picture.
-            }
-        })
+        return res
+            .status(200)
+            .cookie("access_token", token, {
+                httpOnly: true, // TODO: change this later when HTTPS is introduced.
+                secure: true,
+                sameSite: true,
+            })
+            .json({
+                loggedIn: true,
+                user: {
+                    username: saved.username,
+                    email: saved.email,
+                    picture: null, // Since new user have no profile picture.
+                },
+            })
     } catch (err) {
         console.log(err)
 
@@ -152,43 +207,99 @@ register = async (req, res) => {
         return res.status(401).json({
             loggedIn: false,
             user: null,
-            errorMessage: "register backend"
+            errorMessage: "register backend",
         })
     }
 }
 
-
-// To be implemented in later builds
-
 logout = async (req, res) => {
-    // Logsout
-    // Invalid session
-
-    return res.status(200).json({
-        // Set cookie to expire
-    })
+    // This should be enough... front end needs to redirect to homepage.
+    return res.clearCookie("access_token").status(200)
 }
+
+// TODO: To be implemented once email service is setup.
 
 resetRequest = async (req, res) => {
     // Get info from req
-
     // Send to auth manager
-
     // Return status
 }
 
 verifyCode = async (req, res) => {
     // Get code from req
-
     // Send to auth manager
-
     // Return status
 }
 
 updatePasscode = async (req, res) => {}
-updateUsername = async (req, res) => {}
-updateEmail = async (req, res) => {}
-deleteAccount = async (req, res) => {}
+
+// TODO: update documentation to remove login token from the following functions.
+updateUsername = async (req, res) => {
+    try {
+        const { newUsername } = req.body
+
+        var existingUser = await User.findOne({ username: newUsername })
+        if (existingUser) {
+            return res.status(401).json({
+                errorMessage: "Username not unique.",
+            })
+        }
+
+        var targetUser = await User.findOneAndUpdate(
+            { _id: res.locals.userId },
+            { username: newUsername }
+        )
+
+        if (!targetUser) {
+            // This happens if the database went down during the request.
+            return res.status(500)
+        }
+
+        return res.status(200)
+    } catch (err) {
+        return res.status(500)
+    }
+}
+
+updateEmail = async (req, res) => {
+    try {
+        const { newEmail } = req.body
+
+        var existingUser = await User.findOne({ username: newEmail })
+        if (existingUser) {
+            return res.status(401).json({
+                errorMessage: "Email not unique.",
+            })
+        }
+
+        var targetUser = await User.findOneAndUpdate(
+            { _id: res.locals.userId },
+            { email: newEmail }
+        )
+
+        if (!targetUser) {
+            // This happens if the database went down during the request.
+            return res.status(500)
+        }
+
+        return res.status(200)
+    } catch (err) {
+        return res.status(500)
+    }
+}
+
+deleteAccount = async (req, res) => {
+    try {
+        var deleteUser = await User.findOne({ _id: res.locals.userId })
+        if (deleteUser.ok === 1) {
+            return res.status(200)
+        } else {
+            return res.status(401) // TODO: maybe change to 500?
+        }
+    } catch (err) {
+        return res.status(500)
+    }
+}
 
 module.exports = {
     loggedIn,
@@ -200,5 +311,5 @@ module.exports = {
     updatePasscode,
     updateUsername,
     updateEmail,
-    deleteAccount
+    deleteAccount,
 }
