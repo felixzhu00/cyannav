0dfca18c237db7f8beb230e9bbb7706898ecc1a6
Tommy <lin.tommy@outlook.com>
2023-11-18 03:09:38 -0500

    Added color palette

--- client/src/App.js
+++ client/src/App.js
@@ -24,15 +24,54 @@ import BrowsePage from './components/BrowsePage.jsx';
 import MapViewingPage from './components/MapViewingPage.jsx';
 import React, { useState, useRef, useEffect } from 'react';
 
+// Define your color palette
+const colors = {
+  primary: {
+    100: '#00CED1',
+    200: '#00b0b3',
+    300: '#006d72',
+  },
+  accent: {
+    100: '#20B2AA',
+    200: '#00544f',
+  },
+  text: {
+    100: '#333333',
+    200: '#5c5c5c',
+  },
+  background: {
+    100: '#E0FFFF',
+    200: '#d6f5f5',
+    300: '#aecccc',
+  },
+};
 
+// Create a theme instance
 const theme = createTheme({
+  palette: {
+    primary: {
+      main: colors.primary[100],
+      light: colors.primary[200],
+      dark: colors.primary[300],
+    },
+    secondary: {
+      main: colors.accent[100],
+      dark: colors.accent[200],
+    },
+    text: {
+      primary: colors.text[100],
+      secondary: colors.text[200],
+    },
+    background: {
+      default: colors.background[100],
+      paper: colors.background[200],
+      dark: colors.background[300],
+    },
+  },
   typography: {
-    fontFamily: [
-      'Inter',
-    ].join(','),
+    fontFamily: 'Inter',
   },
 });
-
 
 const App = () => {
 
@@ -44,10 +83,10 @@ const App = () => {
   return (
     <BrowserRouter>
       <ThemeProvider theme={theme}>
-        <AppBanner guest={guest}/>
+        <AppBanner guest={guest} />
         <Routes>
-          <Route path="/" element={<LoginScreen handleGuest={setGuest}/>} />
-          <Route path="/login/" element={<LoginScreen handleGuest={setGuest}/>} />
+          <Route path="/" element={<LoginScreen handleGuest={setGuest} />} />
+          <Route path="/login/" element={<LoginScreen handleGuest={setGuest} />} />
           <Route path="/register/" element={<RegisterScreen />} />
           <Route path="/forget/" element={<ForgetPswdScreen />} />
           <Route path="/profile/" element={<ProfileScreen />} />
