f661b44118edbf57cc4baf2184e8b54dc66b2be1
yuke <yuke@cs.stonybrook.edu>
2023-11-17 19:35:11 -0500

    Made api routes more consistent

--- server/routes/api-router.js
+++ server/routes/api-router.js
@@ -1,43 +1,70 @@
-const express = require('express')
+const express = require("express")
 const router = express.Router()
-const mapGraphicSchemaController = require('../controllers/api-controller')
-const auth = require('../auth') // auth_manager
-
+const mapGraphicSchemaController = require("../controllers/api-controller")
+const auth = require("../auth") // auth_manager
 
 //Handles getting an existing map(by ID) request
-router.get('/api/map:id', auth.verify, mapGraphicSchemaController.getMapById)
+router.get("/map/:id", auth.verify, mapGraphicSchemaController.getMapById)
 
 //Handles getting all maps made by a specific user request
-router.get('/api/user:userId', auth.verify, mapGraphicSchemaController.getUserMaps)
- 
-//Handles getting 
-router.get('/api/allmap', auth.verify, mapGraphicSchemaController.getAllMaps) 
+router.get(
+    "/mapbyuser/:id",
+    auth.verify,
+    mapGraphicSchemaController.getUserMaps
+)
 
-//Handles getting all existing maps request
-router.get('/api/mapjson:id', auth.verify, mapGraphicSchemaController.getMapJsonById)
+//Handles getting all maps
+router.get("/allmap", auth.verify, mapGraphicSchemaController.getAllMaps)
+
+//Handles getting the geojson of the map
+router.get(
+    "/mapjson/:id",
+    auth.verify,
+    mapGraphicSchemaController.getMapJsonById
+)
 
 //Handles creating a new map request
-router.post('/api/newmap', auth.verify, mapGraphicSchemaController.createNewMap)
+router.post("/newmap", auth.verify, mapGraphicSchemaController.createNewMap)
 
 //Handles duplicating a map(by ID) request
-router.post('/api/duplicatemap', auth.verify, mapGraphicSchemaController.createDuplicateMapById)
+router.post(
+    "/duplicatemap",
+    auth.verify,
+    mapGraphicSchemaController.createDuplicateMapById
+)
 
 //Handles forking a map(by ID) request
-router.post('/api/forkmap', auth.verify, mapGraphicSchemaController.createForkMapById)
+router.post(
+    "/forkmap",
+    auth.verify,
+    mapGraphicSchemaController.createForkMapById
+)
 
 //Handles deleting map(by ID) request
-router.delete('/map:id', auth.verify, mapGraphicSchemaController.deleteMapById)
+router.delete(
+    "/deletemap/:id",
+    auth.verify,
+    mapGraphicSchemaController.deleteMapById
+)
 
 //Handles updating the name of a map (by ID) request
-router.put('/api/mapname:id', auth.verify, mapGraphicSchemaController.updateMapNameById)
+router.post(
+    "/mapname",
+    auth.verify,
+    mapGraphicSchemaController.updateMapNameById
+)
 
 //Handles updating a map's tags request
-router.put('/api/maptag:id', auth.verify, mapGraphicSchemaController.updateMapTag)
+router.post("/maptag", auth.verify, mapGraphicSchemaController.updateMapTag)
 
 //Handles updating a map's publish status request(created? unfinished? published? etc)
-router.put('/api/mapstatus', auth.verify, mapGraphicSchemaController.updateMapPublishStatus)
+router.post(
+    "/mapstatus",
+    auth.verify,
+    mapGraphicSchemaController.updateMapPublishStatus
+)
 
 //Handles updating a map's json when edited request
-router.put('/api/mapjson:id', auth.verify, mapGraphicSchemaController.updateMapJson)
+router.post("/mapjson", auth.verify, mapGraphicSchemaController.updateMapJson)
 
 module.exports = router
