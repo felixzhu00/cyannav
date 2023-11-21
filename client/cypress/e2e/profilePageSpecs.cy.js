5dfb9cb7a1f4e5196f73610dba8b2eaacbff58cb
Robert <robert.buthorn@stonybrook.edu>
2023-11-21 11:15:14 -0500

    fully running test cases

--- client/cypress/e2e/profilePageSpecs.cy.js
+++ client/cypress/e2e/profilePageSpecs.cy.js
@@ -1,30 +1,19 @@
 describe("profile page specs", () => {
 
-    Cypress.Commands.add('register', (email, username, password, verifyPassword) => {
-        cy.session([email, username, password, verifyPassword], () => {
-          cy.visit("localhost:3000")
-          cy.get("#registerLink").click()
-          cy.get('#email').type(email)
-          cy.get('#username').type(username)
-          cy.get('#password').type(password)
-          cy.get('#verify-password').type(verifyPassword)
-          cy.get("#registerBtn").click()
-        })
-    })
-    Cypress.Commands.add('login', (email, password) => {
-        cy.session([email, password], () => {
-          cy.visit("localhost:3000")
-          cy.get('#email').type(email)
-          cy.get('#password').type(password)
-          cy.get("#signInBtn").click()
-        })
-    })
     beforeEach(() => {
-        cy.register("autotest", "autotest", "11223344!!", "11223344!!")
+        cy.visit("localhost:3000")
+        cy.get("#registerLink").click()
+        cy.get('#email').type("autotest")
+        cy.get('#username').type("autotest")
+        cy.get('#password').type("11223344!!")
+        cy.get('#verify-password').type("11223344!!")
+        cy.get("#registerBtn").click()
+        cy.get('#email').type("autotest")
+        cy.get('#password').type("11223344!!")
         cy.get("#signInBtn").click()
-        cy.login("autotest", "11223344!!")
+        cy.wait(500)
         cy.get("#settingsDropdown").click()
-        cy.get("#settingsDropdownOption").first().click()
+        cy.get("#settingsDropdownOption").click()
     })
     it("displays the change email modal when corresponding button is clicked", () => {
         cy.get("#emailEditBtn").click()
