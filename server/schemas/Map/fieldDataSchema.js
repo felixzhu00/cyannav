a499fc435b2d232320280ac7a7ce1b0a8c1dff3d
yuke <yuke@cs.stonybrook.edu>
2023-11-25 20:53:57 -0500

    To be implemented, dummy code

--- server/schemas/Map/fieldDataSchema.js
+++ server/schemas/Map/fieldDataSchema.js
@@ -3,6 +3,10 @@ const Schema = mongoose.Schema
 
 const fieldDataSchema = new Schema({
     // TODO: (build4) TO BE IMPLEMENTED
+    geoBuf: {
+        type: Buffer,
+        required: true,
+    },
 })
 
 const fieldDataModel = mongoose.model("fieldData", fieldDataSchema)
