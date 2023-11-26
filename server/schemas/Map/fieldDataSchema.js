3d12afb781e598edff56b189d7515f65aff98e18
yuke <yuke@cs.stonybrook.edu>
2023-11-25 20:32:19 -0500

    Consistent naming

--- server/schemas/Map/fieldDataSchema.js
+++ server/schemas/Map/fieldDataSchema.js
@@ -5,5 +5,5 @@ const fieldDataSchema = new Schema({
     // TODO: (build4) TO BE IMPLEMENTED
 })
 
-const mapGraphicModel = mongoose.model("fieldData", fieldDataSchema)
-module.exports = mapGraphicModel
+const fieldDataModel = mongoose.model("fieldData", fieldDataSchema)
+module.exports = fieldDataModel
