857b8b11eb636ad5eec1694fdced626c9c902c3a
yuke <yuke@cs.stonybrook.edu>
2023-11-25 20:33:42 -0500

    Point db to build4

--- server/db/index.js
+++ server/db/index.js
@@ -1,11 +1,9 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose")
 
 // TODO: use environmental variables later.
-mongoose.connect(
-    'mongodb://127.0.0.1:27017/build1'
-    ).catch(e => {
-        console.error("Connection error:" , e.message)
-    })
+mongoose.connect("mongodb://127.0.0.1:27017/build4").catch((e) => {
+    console.error("Connection error:", e.message)
+})
 
 const db = mongoose.connection
 
