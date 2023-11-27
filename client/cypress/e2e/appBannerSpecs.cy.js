76b9fa60b53fda477731d38e5e4891141cda1a16
Robert <robert.buthorn@stonybrook.edu>
2023-11-27 11:52:19 -0500

    testing

--- client/cypress/e2e/appBannerSpecs.cy.js
+++ client/cypress/e2e/appBannerSpecs.cy.js
@@ -1,21 +1,20 @@
 describe('app banner specs', () => {
     before(() => {
-        cy.registerUser("autotest5", "autotest5", "11223344&&", "11223344&&")
+        cy.registerUser("autotest10", "autotest10", "11223344&&", "11223344&&")
         cy.wait(2000)
-        cy.get("#settingsDropdown").click()
-        cy.get("#settingsDropdownOption").first().click()
-        cy.get("#logoutBtn").click()
     })
 
     beforeEach(() => {
-        cy.signInUser("autotest5", "11223344&&")
+        cy.visit("129.213.145.105")
     })
   
     it('should have /browsepage in the url', () => {
+        cy.visit("129.213.145.105")
       cy.url().should('include', '/browsepage')
     })
   
     it("marketplace button routes to browse page", () => {
+        cy.signInUser("autotest10", "11223344&&")
         cy.get("#settingsDropdown").click()
         cy.get("#settingsDropdownOption").first().click()
         cy.get("#marketplaceBtn").click()
@@ -23,6 +22,7 @@ describe('app banner specs', () => {
     })
 
     it("logout button routes to login page", () => {
+        cy.signInUser("autotest10", "11223344&&")
         cy.get("#settingsDropdown").click()
         cy.get("#settingsDropdownOption").first().click()
         cy.get("#logoutBtn").click()
@@ -30,12 +30,14 @@ describe('app banner specs', () => {
     })
 
     it("account settings button routes to profile page", () => {
+        cy.signInUser("autotest10", "11223344&&")
         cy.get("#settingsDropdown").click()
         cy.get("#settingsDropdownOption").first().click()
         cy.url().should('include', '/profile')
     })
 
     it("logo button routes to browse page", () => {
+        cy.signInUser("autotest10", "11223344&&")
         cy.get("#settingsDropdown").click()
         cy.get("#settingsDropdownOption").first().click()
         cy.get("#logoBtn").click()
