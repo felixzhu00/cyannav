b8aac2b5d2cac9162d4662e5a50ef2727eb590ae
yuke <yuke@cs.stonybrook.edu>
2023-12-04 01:15:18 -0500

    made front-end test run on localhost:3000

--- client/cypress/e2e/appBannerSpecs.cy.js
+++ client/cypress/e2e/appBannerSpecs.cy.js
@@ -1,24 +1,24 @@
-describe('app banner specs', () => {
+describe("app banner specs", () => {
     before(() => {
         cy.registerUser("autotest13", "autotest13", "11223344&&", "11223344&&")
         cy.wait(2000)
     })
 
     beforeEach(() => {
-        cy.visit("129.213.145.105")
+        cy.visit("localhost:3000")
     })
-  
-    it('should have /browsepage in the url', () => {
-        cy.visit("129.213.145.105")
-      cy.url().should('include', '/browsepage')
+
+    it("should have /browsepage in the url", () => {
+        cy.visit("localhost:3000")
+        cy.url().should("include", "/browsepage")
     })
-  
+
     it("marketplace button routes to browse page", () => {
         cy.signInUser("autotest13", "11223344&&")
         cy.get("#settingsDropdown").click()
         cy.get("#settingsDropdownOption").first().click()
         cy.get("#marketplaceBtn").click()
-        cy.url().should('include', '/browsepage')
+        cy.url().should("include", "/browsepage")
     })
 
     it("logout button routes to login page", () => {
@@ -26,14 +26,14 @@ describe('app banner specs', () => {
         cy.get("#settingsDropdown").click()
         cy.get("#settingsDropdownOption").first().click()
         cy.get("#logoutBtn").click()
-        cy.url().should('include', '/login')
+        cy.url().should("include", "/login")
     })
 
     it("account settings button routes to profile page", () => {
         cy.signInUser("autotest10", "11223344&&")
         cy.get("#settingsDropdown").click()
         cy.get("#settingsDropdownOption").first().click()
-        cy.url().should('include', '/profile')
+        cy.url().should("include", "/profile")
     })
 
     it("logo button routes to browse page", () => {
@@ -41,7 +41,6 @@ describe('app banner specs', () => {
         cy.get("#settingsDropdown").click()
         cy.get("#settingsDropdownOption").first().click()
         cy.get("#logoBtn").click()
-        cy.url().should('include', '/browsepage')
+        cy.url().should("include", "/browsepage")
     })
-  })
-  
+})
