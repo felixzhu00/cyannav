7c794e06dda2528171e14c7f7bc0e0ad18f3f50f
yuke <yuke@cs.stonybrook.edu>
2023-12-16 18:47:56 -0500

    Fix title being an array of string instead just 1 string

--- server/schemas/Map/mapMetadataSchema.js
+++ server/schemas/Map/mapMetadataSchema.js
@@ -2,12 +2,10 @@ const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 
 const mapMetadataSchema = new Schema({
-    title: [
-        {
-            type: String,
-            required: [true, "No title has been provided"],
-        },
-    ],
+    title: {
+        type: String,
+        required: [true, "No title has been provided"],
+    },
     user: [
         {
             type: Schema.Types.ObjectID,
