ff4602c90811c99f83db3dafc80db1bc38edf42a
felixzhu00 <felix.zhu00@gmail.com>
2023-12-19 02:45:32 -0500

    complete DFM, add more feature to pointmap, 3d map

--- client/src/components/NavJSON.jsx
+++ client/src/components/NavJSON.jsx
@@ -1,19 +1,18 @@
 import L from "leaflet";
 import { interpolateRgb } from "d3-interpolate";
 import HeatmapOverlay from "../leaflet-heatmap.js";
-import * as turf from "@turf/turf";
+// import * as turf from "@turf/turf";
 import { useRef, useEffect, useState, useContext } from "react";
 import { GlobalStoreContext } from "../store";
+import tinycolor from "tinycolor2";
 
 function NavJSON({ data, center, zoom }) {
     const { store } = useContext(GlobalStoreContext);
     const geolayer = useRef(null);
     const heatmap = useRef(null);
     const map = useRef(null);
-    const min = useRef(null);
-    const max = useRef(null);
 
-    const onEachFeature = (country, layer) => {
+    const onEachFeature = (country, layer, isPicking=false) => {
         const name1 = country?.fields?.immutable?.name;
         const name2 = country.properties && country.properties.admin;
         const name3 = country.properties && country.properties.NAME_0;
@@ -21,7 +20,7 @@ function NavJSON({ data, center, zoom }) {
         const name = name1 || name2 || name3 || "";
 
         layer.bindPopup(name);
-
+        const { min, max } = findMinMax(data);
         // Possible map templates : 'heatmap', 'distributiveflowmap', 'pointmap', 'choroplethmap', '3drectangle'
 
         // Set the style for every map
@@ -42,18 +41,54 @@ function NavJSON({ data, center, zoom }) {
                         ? "#007dff"
                         : getColor(
                               country.fields.mutable[store.byFeature],
-                              min.current,
-                              max.current,
+                              min,
+                              max,
                               colors?.colorA,
                               colors?.colorB
                           ),
             });
         }
         // Control the styling when selecting a certain feature(area)
+
+        if (isPicking) {
+            layer.on({
+                mouseover: highlightFeature,
+                mouseout: resetHighlight,
+            });
+        }
         layer.on({
             click: toggleSelection,
         });
     };
+
+    // When hover
+    function highlightFeature(e) {
+        var layer = e.target;
+
+        layer.setStyle({
+            weight: 5,
+            color: "#666",
+            dashArray: "",
+            fillOpacity: 0.7,
+        });
+
+        layer.bringToFront();
+    }
+
+    //When unhover
+    function resetHighlight(e) {
+        var layer = e.target;
+
+        layer.setStyle({
+            weight: 2,
+            opacity: 1,
+            color: "white",
+            dashArray: "3",
+            fillOpacity: store.currentMap.mapType == "heatmap" ? 0 : 0.7,
+        });
+
+        // geolayer.current.resetStyle(e.target);
+    }
 
     const toggleSelection = (e) => {
         const layer = e.target;
@@ -82,6 +117,7 @@ function NavJSON({ data, center, zoom }) {
             }
         });
 
+
         if (!isLayerSelected) {
             // Revert to original style
             layer.setStyle({
@@ -92,7 +128,6 @@ function NavJSON({ data, center, zoom }) {
                 fillOpacity: store.currentMap.mapType == "heatmap" ? 0 : 0.7,
             });
             store.setCurrentArea(-1);
-            return null;
         } else {
             // Set the selected style
             layer.setStyle({
@@ -101,7 +136,11 @@ function NavJSON({ data, center, zoom }) {
                 dashArray: "",
                 fillOpacity: store.currentMap.mapType == "heatmap" ? 0 : 0.7,
             });
-            layer.bringToFront();
+            // layer.bringToFront();
+            store.setCurrentArea(layerIndex);
+        }
+
+        if (store.isPickingDFM) {
             store.setCurrentArea(layerIndex);
         }
     };
@@ -136,18 +175,20 @@ function NavJSON({ data, center, zoom }) {
     };
 
     // Called in Choropleth Map Only
-    const setupGeoJSONLayer = (geoData) => {
+    const setupGeoJSONLayer = (geoData, isPicking = false) => {
         if (map.current && geolayer.current) {
             // Remove the existing GeoJSON layer
             geolayer.current.clearLayers();
 
             // Create and add the new GeoJSON layer
             const newGeo = L.geoJson(geoData, {
-                onEachFeature: onEachFeature,
+                onEachFeature: (feature, layer) =>
+                    onEachFeature(feature, layer, isPicking),
             }).addTo(map.current);
 
             // Update the geolayer ref
             geolayer.current = newGeo;
+            geolayer.current.bringToBack();
         }
     };
 
@@ -156,7 +197,8 @@ function NavJSON({ data, center, zoom }) {
         const radius = Number(
             store.geojson.features[0]?.fields?.immutable?.radius
         );
-        if (isNaN(radius)) {
+        console.log(radius);
+        if (isNaN(radius) || !store.byFeature) {
             return;
         }
         const cfg = {
@@ -179,8 +221,6 @@ function NavJSON({ data, center, zoom }) {
         // Extract heatmap data from store.geojson
         const heatmapLayerData = store.geojson.features.reduce(
             (acc, feature) => {
-                const center = turf.centerOfMass(feature);
-
                 // Check if the intensity value is available and not NaN
                 const intensity = feature?.fields?.mutable[store.byFeature]
                     ? parseInt(feature.fields.mutable[store.byFeature], 10)
@@ -189,8 +229,8 @@ function NavJSON({ data, center, zoom }) {
                 // Only push if intensity is a valid number
                 if (!isNaN(intensity)) {
                     acc.push({
-                        lat: center.geometry.coordinates[1],
-                        lng: center.geometry.coordinates[0],
+                        lat: feature.fields.immutable.center.latitude,
+                        lng: feature.fields.immutable.center.longitude,
                         count: intensity,
                     });
                 }
@@ -212,6 +252,15 @@ function NavJSON({ data, center, zoom }) {
             return;
         }
 
+        const hexColorPattern = /^#(?:[0-9a-fA-F]{3}){1,2}(?:[0-9a-fA-F]{2})?$/;
+        const color1 =
+            store.geojson.features[0]?.fields?.immutable?.color?.colorA;
+        const color2 =
+            store.geojson.features[0]?.fields?.immutable?.color?.colorB;
+        if (!hexColorPattern.test(color1) && !hexColorPattern.test(color2)) {
+            return;
+        }
+
         if (map.current) {
             // Remove existing circles if any
             map.current.eachLayer((layer) => {
@@ -223,8 +272,6 @@ function NavJSON({ data, center, zoom }) {
 
         // Extract circle data from store.geojson
         store.geojson.features.forEach((feature) => {
-            const center = turf.centerOfMass(feature);
-
             const count = feature?.fields.mutable[store.byFeature]
                 ? parseInt(feature.fields.mutable[store.byFeature], 10)
                 : NaN;
@@ -234,14 +281,14 @@ function NavJSON({ data, center, zoom }) {
                 // Create and add circle marker
                 const circle = L.circle(
                     [
-                        center.geometry.coordinates[1],
-                        center.geometry.coordinates[0],
+                        feature.fields.immutable.center.latitude,
+                        feature.fields.immutable.center.longitude,
                     ],
                     {
-                        radius: count * radius, // Adjust the radius based on the count
-                        fillColor: "gray", // Customize the fill color
+                        radius: Math.min(Math.max(count * radius, 0), 1800000), // Adjust the radius based on the count
+                        fillColor: feature.fields.immutable.color.colorA, // Customize the fill color
                         fillOpacity: 0.8,
-                        color: "black",
+                        color: feature.fields.immutable.color.colorB,
                         weight: 1,
                     }
                 ).addTo(map.current);
@@ -261,6 +308,20 @@ function NavJSON({ data, center, zoom }) {
             return;
         }
 
+        const hexColorPattern = /^#(?:[0-9a-fA-F]{3}){1,2}(?:[0-9a-fA-F]{2})?$/;
+        const color1 =
+            store.geojson.features[0]?.fields?.immutable?.color?.colorA;
+        const color2 =
+            store.geojson.features[0]?.fields?.immutable?.color?.colorB;
+        if (!hexColorPattern.test(color1) && !hexColorPattern.test(color2)) {
+            return;
+        }
+        const color = tinycolor(color1);
+
+        const shade1 = color.darken(0).toString();
+        const shade2 = color.darken(10).toString();
+        const shade3 = color.darken(20).toString();
+
         if (map.current) {
             // Remove existing icon markers if any
             map.current.eachLayer((layer) => {
@@ -272,8 +333,6 @@ function NavJSON({ data, center, zoom }) {
 
         // Extract cube data from store.geojson
         store.geojson.features.forEach((feature) => {
-            const center = turf.centerOfMass(feature);
-
             // Check if the count value is available and not NaN
             const count = feature?.fields?.mutable[store.byFeature]
                 ? parseInt(feature.fields.mutable[store.byFeature], 10)
@@ -283,111 +342,101 @@ function NavJSON({ data, center, zoom }) {
             if (!isNaN(count)) {
                 // Create and add cube marker
 
-                const cubeSize = 50; // Adjust the size as needed
+                // const cubeSize = 50; // Adjust the size as needed
                 const marker = L.marker(
                     [
-                        center.geometry.coordinates[1],
-                        center.geometry.coordinates[0],
+                        feature.fields.immutable.center.latitude,
+                        feature.fields.immutable.center.longitude,
                     ],
                     {
-                        icon: createCubeIcon(cubeSize, count),
+                        icon: createCubeIcon(
+                            scale,
+                            count,
+                            shade1,
+                            shade2,
+                            shade3,
+                            color2
+                        ),
                     }
                 ).addTo(map.current);
             }
         });
     };
 
-    const setupDFM = () => {
-        // Preliminary checking
-        const weight = Number(
-            store.geojson.features[0]?.fields?.immutable?.weight
-        );
-        if (isNaN(weight)) {
-            return;
-        }
 
+    const setupDFM = () => {
         if (map.current) {
-            // Remove existing icon markers if any
+            // Remove existing icon markers that are not in geolayer.current
             map.current.eachLayer((layer) => {
-                if (layer instanceof L.Polyline) {
+                if (layer instanceof L.Polyline && !geolayer.current.hasLayer(layer)) {
                     map.current.removeLayer(layer);
                 }
             });
         }
-
-        // Extract cube data from store.geojson
+    
         store.geojson.features.forEach((feature) => {
             const center = [
-                feature.fields.center_longitude,
-                feature.fields.center_latitude,
+                
+                feature.fields.immutable.center.latitude,
+                feature.fields.immutable.center.longitude,
             ];
-
-            feature.fields.lines.forEach((lines) => {
-                [country_index, line_weight] = lines;
-                const target_country =
-                    store.geojson.features[country_index].fields;
-                const target_center = [
-                    target_country.center_longitude,
-                    target_country.center_latitude,
-                ];
-
-                const count = line_weight ? parseInt(line_weight, 10) : NaN;
-
-                let polyline = L.polyline([center, target_center], {
-                    color: "red",
-                    weight: count,
-                    opacity: 0.5,
-                    smoothFactor: 1,
-                }).addTo(map);
-            });
-
-            // Check if the count value is available and not NaN
-            // const count =
-            //   feature.fields &&
-            //   (feature.fields[store.byFeature]
-            //     ? parseInt(feature.fields[store.byFeature], 10)
-            //     : NaN);
-
-            // Only proceed if count is a valid number
-            if (!isNaN(count)) {
-                // Create and add cube marker
-
-                console.log("count", count, feature.fields);
-                const cubeSize = 50; // Adjust the size as needed
-                const marker = L.marker(
-                    [
-                        center.geometry.coordinates[1],
-                        center.geometry.coordinates[0],
-                    ],
-                    {
-                        icon: createCubeIcon(cubeSize, count),
+    
+            Object.entries(feature.fields.mutable).forEach(([key, value]) => {
+                const match = key.match(/^(\d+)_(.+)/);
+                if (match) {
+                    // Extract relevant information from the matched key
+                    const [, country_index, ] = match;
+                    const target_country =
+                        store.geojson.features[country_index]?.fields?.immutable;
+                    if (target_country) {
+                        const target_center = [
+                            
+                            target_country.center.latitude,
+                            target_country.center.longitude,
+                        ];
+                        const count = value ? parseInt(value, 10) : NaN;
+    
+                        // Create polyline based on extracted information
+                        let polyline = L.polyline([center, target_center], {
+                            color: "red",
+                            weight: count,
+                            opacity: 0.5,
+                            smoothFactor: 1,
+                        }).addTo(map.current);
                     }
-                ).addTo(map.current);
-            }
+                }
+            });
         });
     };
 
-    function createCubeIcon(cubeSize, numCubes) {
+    function createCubeIcon(
+        cubeSize,
+        numCubes,
+        shade1,
+        shade2,
+        shade3,
+        borderColor
+    ) {
         let iconHtml = "<div class='container'>";
 
         for (let i = 0; i < numCubes; i++) {
             // Calculate the offset for the right face
 
             // Add inline styles for each left and right face
-
             iconHtml += `
-        <div class='left' style="transform: translateY(-${
-            i * 100
-        }%) rotate(90deg) skewX(-30deg) scaleY(0.864);"></div>
-        <div class='right' style="transform: translateY(-${
-            i * 100
-        }%) rotate(-30deg) skewX(-30deg) scaleY(0.864);"></div>
-      `;
+                <div class='left' style="transform: translateY(-${
+                    i * 100
+                }%) rotate(90deg) skewX(-30deg) scaleY(0.864); background-color: ${shade2}; border: 1px solid ${borderColor};"></div>
+                <div class='right' style="transform: translateY(-${
+                    i * 100
+                }%) rotate(-30deg) skewX(-30deg) scaleY(0.864); background-color: ${shade3}; border: 1px solid ${borderColor};"></div>
+            `;
         }
 
         // Add the common top face
-        iconHtml +=
-            "<div class='top' style='bottom: " + numCubes * 200 + "%'></div>";
+        iconHtml += `<div class='top' style='bottom: ${
+            numCubes * 200
+        }%; background-color: ${shade1}; border: 1px solid ${borderColor};'></div>`;
         iconHtml += "</div>";
 
         return L.divIcon({
@@ -397,6 +446,21 @@ function NavJSON({ data, center, zoom }) {
             iconAnchor: [0, cubeSize * numCubes],
         });
     }
+
+    const updateMap = () => {
+        setupGeoJSONLayer(data, store.isPickingDFM);
+        // Possible map templates: 'heatmap', 'distributiveflowmap', 'pointmap', 'choroplethmap', '3drectangle'
+        if (store.currentMap.mapType === "heatmap") {
+            setupHeatMapLayer();
+        } else if (store.currentMap.mapType === "pointmap") {
+            setupPointMapLayer();
+        } else if (store.currentMap.mapType === "distributiveflowmap") {
+            setupDFM();
+        } else if (store.currentMap.mapType === "3drectangle") {
+            setup3D();
+        }
+    }
+
 
     useEffect(() => {
         // Initialize the Leaflet map and store its reference in the map ref
@@ -417,6 +481,7 @@ function NavJSON({ data, center, zoom }) {
         const geo = L.geoJson(data, {
             onEachFeature: onEachFeature,
         }).addTo(mapInstance);
+        // geo.bringToBack()
 
         geolayer.current = geo;
 
@@ -429,6 +494,7 @@ function NavJSON({ data, center, zoom }) {
                 map.current.getBoundsZoom(geoBounds) + 1
             );
         }
+        updateMap()
 
         // Cleanup function to remove the map and associated layers when the component is unmounted
         return () => {
@@ -439,21 +505,8 @@ function NavJSON({ data, center, zoom }) {
     // Use another useEffect to set the view after the initial render
     useEffect(() => {
         // Calcluate the min and max
-        const { small, big } = findMinMax(data);
-        min.current = small;
-        max.current = big;
-        setupGeoJSONLayer(data);
-        // Possible map templates: 'heatmap', 'distributiveflowmap', 'pointmap', 'choroplethmap', '3drectangle'
-        if (store.currentMap.mapType === "heatmap") {
-            setupHeatMapLayer();
-        } else if (store.currentMap.mapType === "pointmap") {
-            setupPointMapLayer();
-        } else if (store.currentMap.mapType === "distributiveflowmap") {
-            setupDFM();
-        } else if (store.currentMap.mapType === "3drectangle") {
-            setup3D();
-        }
-    }, [data]);
+        updateMap()
+    }, [data, store.isPickingDFM]);
 
     useEffect(() => {
         setupGeoJSONLayer(data);
